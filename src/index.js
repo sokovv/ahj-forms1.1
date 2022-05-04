c9633afc94ad8841228b2a12f2a92f0b31f4df1b
sokovv <88129553+sokovv@users.noreply.github.com>
2022-05-04 04:08:36 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable linebreak-style */
-import './css/style.css';
-
-import './js/app';
-
-// TODO: write your code in app.js
--- src/js/app.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable linebreak-style */
-import ToolTripWidget from './toolTrip';
-
-const widget = new ToolTripWidget(document.querySelector('.block'));
-widget.bindToDOM();
